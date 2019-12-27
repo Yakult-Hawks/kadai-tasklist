8c518a6783137adbd273526192e4a429343dd860
Yakult-Hawks <osca1102@gmail.com>
2019-12-27 07:38:18 +0000

    Complete Creating Controller "TasksController"

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    //一覧表示処理
+    public function index()
+    {
+        //
+    }
+
+    //新規登録画面表示処理
+    public function create()
+    {
+        //
+    }
+
+    //新規登録処理
+    public function store(Request $request)
+    {
+        //
+    }
+
+    //取得表示処理
+    public function show($id)
+    {
+        //
+    }
+
+    //更新画面表示処理
+        public function edit($id)
+    {
+        //
+    }
+
+    //更新処理
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    //削除処理
+        public function destroy($id)
+    {
+        //
+    }
+}
